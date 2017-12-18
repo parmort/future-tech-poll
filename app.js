ce40ca084761aed53892710ca1a97d4cfc0be24b
Saturnus <parvus.mortalis@gmail.com>
2017-12-18 15:31:39 -0500

    Fix start script for Heroku

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ app.set('view engine', 'pug');
 // Stylesheet Setup
 app.use("/css", express.static(__dirname + "/css"));
 
+// Vars
+const copr = "Copr. 2017 © N. Prochnau. All Rights Reserved."
+
 app.get('/', (req, res) => {
-    res.render('index', {copr: "Copr. 2017 © N. Prochnau. All Rights Reserved."});
+    res.render('index', {copr: copr});
 });
 
 app.listen(3000, () => console.log('Server started on Port 3000'));
